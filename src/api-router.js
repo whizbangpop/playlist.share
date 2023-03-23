4b1496a001aef152443177ad49dc4a8103162af8
WhizBangPop <abi@quickexit.cc>
2023-03-23 17:04:17 +0000

    route added

--- src/api-router.js
+++ src/api-router.js
@@ -2,26 +2,57 @@ const { Router } = require('express');
 const router = Router();
 const { QuickDB } = require("quick.db");
 const db = new QuickDB();
-const playlistDb = await db.table("playshare_list_db")
+const playlistDb = db.table("playshare_list_db");
+const rateLimiter = require('express-rate-limit');
+
+const allowList = ["194.83.172.180"]
+const newPlayShareLimiter = rateLimiter({
+    windowMs: 30 * 60 * 1000,
+    max: 5,
+    message:
+        'Too many PlayShares created from this IP, please try again in 30 mins',
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
 router.get('/new/*', async (req, res) => {
     const { url, hostname, platform, playlistid } = await getUrlInfo(req)
-    console.log(url)
+    console.log(req.params)
 
     // res.json({ playlistUrl: url, hostname, platform, playlistid })
     // res.render('index', { url, hostname, platform, playlistid })
-    res.redirect(`/playlist/new?plstid=${playlistid}`)
-    playlistDb.push(playlistid, { platform })
+    res.redirect(`/ps/new?plstid=${playlistid}`)
+    playlistDb.push(`${playlistid}`, { platform, origin_url: hostname })
     console.log(platform)
 })
 
-router.get('/playlist/new', async (req, res) => {
-    console.log(req.query)
-    res.render('new-playlist', { platform: req.query.platform, plstid: req.query.plstid })
+router.get('/ps/new', async (req, res) => {
+    try {
+        const playshareList = await playlistDb.get(`${req.query.plstid}`)
+        if (!playshareList || playshareList[0].platform === undefined) {
+            return res.render('creation-failed')
+        }
+        res.render('new-playlist', { plstid: req.query.plstid, platform: `${playshareList[0].platform}`, origin: `${playshareList[0].origin_url}` })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
-router.get('/ps/:playshareid', async (req, res) => {
+router.get('/ps/info/*', async (req, res) => {
+    try {
+        const shareList = await playlistDb.get(req.params[0])
+        console.log(shareList)
+        if (!shareList || shareList[0].platform === undefined) {
+            return res.render('render-fail')
+        }
+        res.render('playshare-info', { origin: shareList[0].origin_url, plstid: req.params[0] })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
+router.get('/ps/dev-routes/creation-failed', async (req, res) => {
+    res.render('creation-failed')
 })
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const port = 4000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 
+app.use("/assets", express.static('assets'));
+
 app.use('/', require('./api-router'))
 app.use('/dist', require('./view-router'))
 
